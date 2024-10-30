4ccce960de9fe2ccd021a6c46e6f485e53c3c85e
Lukas Balthazar <lbturner@outlook.dk>
2024-10-30 13:27:33 +0100

    repo connection system env + app properties

--- src/main/java/org/example/wishlistkl/Model/WishList.java
+++ src/main/java/org/example/wishlistkl/Model/WishList.java
@@ -1,4 +1,59 @@
 package org.example.wishlistkl.Model;
 
 public class WishList {
+    private String user;
+    private int id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+
+
+    public WishList(String user, int id, String name, String email, String phoneNumber) {
+        this.user = user;
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+
+    }
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    //Evt. Override toString() method.
 }
--- src/main/java/org/example/wishlistkl/Repository/WishListRepository.java
+++ src/main/java/org/example/wishlistkl/Repository/WishListRepository.java
@@ -1,4 +1,21 @@
 package org.example.wishlistkl.Repository;
 
+
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+@Repository
 public class WishListRepository {
+
+    private String url = System.getenv("url");
+    private String user = System.getenv("username");
+    private String password = System.getenv("password");
+
+    // Establishing the database connection
+    private Connection connect() throws SQLException {
+        return DriverManager.getConnection(url, user, password);
+    }
 }
