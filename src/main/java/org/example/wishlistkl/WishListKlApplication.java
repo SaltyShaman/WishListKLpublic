0de32ad559fe383acce25d6f50692aac9ce00027
Lukas Balthazar <LBTurner@outlook.dk>
2024-10-30 12:35:21 +0100

    Initial commit

---
+++ src/main/java/org/example/wishlistkl/WishListKlApplication.java
@@ -0,0 +1,13 @@
+package org.example.wishlistkl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishListKlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WishListKlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/wishlistkl/WishListKlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.wishlistkl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishListKlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
