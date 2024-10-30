6703ce8901227d3c8b54843f9c97dda86324464c
Lukas Balthazar <LBTurner@outlook.dk>
2024-10-30 12:52:17 +0100

    Packages og klasse navne til  service, controller, repository og model

--- src/main/java/org/example/wishlistkl/WishListKlApplication.java
+++ src/main/java/org/example/wishlistkl/WishListKlApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WishListKlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WishListKlApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WishListKlApplication.class, args);
+    }
 
 }
