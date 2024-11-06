de32951aa06f712b846d9da4acf9ba2af97392fe
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-11-06 14:30:39 +0100

    Autowired and importing for controller and service

--- src/main/java/org/example/wishlistkl/Controller/WishListController.java
+++ src/main/java/org/example/wishlistkl/Controller/WishListController.java
@@ -1,8 +1,18 @@
 package org.example.wishlistkl.Controller;
 
 
+import org.example.wishlistkl.Service.WishListService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class WishListController {
+
+    private final WishListService wishListService;
+
+    @Autowired
+    public WishListController(WishListService wishListService) {
+        this.wishListService = wishListService;
+    }
+
 }
--- src/main/java/org/example/wishlistkl/Service/WishListService.java
+++ src/main/java/org/example/wishlistkl/Service/WishListService.java
@@ -1,6 +1,18 @@
 package org.example.wishlistkl.Service;
 
+import org.example.wishlistkl.Repository.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WishListService {
+
+    private final WishListRepository wishListRepository;
+
+    @Autowired
+    public WishListService(WishListRepository wishListRepository) {
+        this.wishListRepository = wishListRepository;
+    }
 
 
 
