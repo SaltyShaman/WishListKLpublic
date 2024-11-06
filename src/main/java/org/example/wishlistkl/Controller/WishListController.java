0e4682d9ad452f7dba17331d8b349b50f8078282
Kristoffer Søholm Gillesberg <kristoffer6400@gmail.com>
2024-11-06 17:00:53 +0100

    Dependencies

--- src/main/java/org/example/wishlistkl/Controller/WishListController.java
+++ src/main/java/org/example/wishlistkl/Controller/WishListController.java
@@ -3,9 +3,15 @@ package org.example.wishlistkl.Controller;
 
 import org.example.wishlistkl.Service.WishListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/wishlists")
 public class WishListController {
 
     private final WishListService wishListService;
@@ -15,4 +21,14 @@ public class WishListController {
         this.wishListService = wishListService;
     }
 
+    @PostMapping
+    public ResponseEntity<String> addWishList(@RequestBody org.example.wishlistkl.model.WishList wishList) {
+        try {
+            wishListService.addWishList(wishList);
+            return ResponseEntity.status(HttpStatus.CREATED).body("Wishlist added successfully for user: " + wishList.getUsername());
+        } catch (Exception e) {
+            e.printStackTrace();  // incase of error a print on error
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error while adding wishlist: " + e.getMessage());
+        }
+    }
 }
